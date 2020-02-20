2339db70343960b1183d3a49980936fffa4add84
Kyle McKenna <mckennakyler@gmail.com>
2020-02-20 17:19:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,10 +2,15 @@ require_relative "../config/environment.rb"
 
 class Student
 
+def initialize (id = nil, name, grade)
+  @id = id
+  @name = name 
+  @grade = grade
+end   
 
-
-
-
+def self.create_table 
+  
+end 
 
 
 
