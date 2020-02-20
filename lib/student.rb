961e43286fff0e6469d09106d5ac99fc6ab3dda9
Kyle McKenna <mckennakyler@gmail.com>
2020-02-20 17:37:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,7 +54,7 @@ def self.find_by_name
 end 
 
 def update
-  sql = "UPDATE students SET name = ?, grade = ? WHERE name = ?"
+  sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.album, self.name)
   end
 end   
