1007235167f9636e0566e6d227066932ae935151
Kyle McKenna <mckennakyler@gmail.com>
2020-02-20 17:28:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,21 @@ def save
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
 end   
 
+def self.create 
+  
+end 
 
+def self.new_from_db
+  
+end 
+
+def self.find_by_name
+  
+end 
+
+def update
+  
+end   
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
